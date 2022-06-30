56872df608feeabeb6683fd9fca3aa1d036eab87
David Fonseca <jcotta@Colsof.com>
2022-06-30 08:49:37 -0500

    Ajuste psql

--- src/controller/entrenador.controller.js
+++ src/controller/entrenador.controller.js
@@ -3,12 +3,12 @@ var bcrypt = require("bcrypt");
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'strength_club',
-  password: 'santafe',
-  port: 5432,
-});
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  });
 
 const getEntrenadores = (request,response) =>{
   pool.query("SELECT nombre,cedula FROM entrenadores",(error,results)=>{
