d9a890430b8ca13121fd1592430ceaba6708439b
David Fonseca <jcotta@Colsof.com>
2022-06-30 08:57:41 -0500

    Ajuste psql

--- src/controller/paquetes.controller.js
+++ src/controller/paquetes.controller.js
@@ -8,6 +8,9 @@
 		database: process.env.PG_DATABASE,
 		password: process.env.PG_PASSWORD,
 		port: process.env.PG_PORT,
+		ssl: {
+		  rejectUnauthorized: false,
+		}
 		});
 
 
--- src/controller/ventas.controller.js
+++ src/controller/ventas.controller.js
@@ -5,12 +5,15 @@ require("dotenv").config();
 
 
 const pool = new Pool({
-user: process.env.PG_USER,
-host: process.env.PG_HOST,
-database: process.env.PG_DATABASE,
-password: process.env.PG_PASSWORD,
-port: process.env.PG_PORT,
-});
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    ssl: {
+      rejectUnauthorized: false,
+    }
+    });
 
 const registrarVentaProductos = async (request, response) => {
     const client = await pool.connect();
