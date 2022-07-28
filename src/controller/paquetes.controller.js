711c0e090d327e070e8a5a9d80535fbc9ec43ca3
David Fonseca <jcotta@Colsof.com>
2022-07-28 17:31:23 -0500

    Ajuste

--- src/controller/paquetes.controller.js
+++ src/controller/paquetes.controller.js
@@ -1,5 +1,6 @@
 	var jwt = require("jsonwebtoken");
 	var bcrypt = require("bcrypt");
+const { ClientBase } = require("pg");
 	const Pool = require("pg").Pool
 
 	const pool = new Pool({
@@ -13,91 +14,153 @@
 		}
 		});
 
-
-
-	const crearPaquete = (request, response) => {
-		let codigoPaquete = request.body.codigo;
-		let nombre = request.body.nombre;
-		let productos = request.body.productos;
-		let precio = request.body.precio;
-		pool.connect((err, client, done)=>{
-			let hayError = false;
-			const shouldAbort = (err) => {
-				if (err) {
-				hayError=true;
-				client.query('ROLLBACK', err2 => {
-					if (err2) {
-						console.log("Error al hacer rollback")
-					}
-				})
-					return true;
-				}
-				else{
-					return false;
-				}
-			}
-
-		if(nombre && productos && precio && codigoPaquete){
-			client.query("BEGIN", (error)=>{
-			if (shouldAbort(error)) {
-					response.status(400).send({
-					message:"Error al crear paquete"
-				});
-					done();
-					return;
-				}else{
-					client.query("INSERT INTO paquetes(codigo,precio,nombre) VALUES($1,$2,$3)",[codigoPaquete,precio,nombre], (error2)=>{
-						if (shouldAbort(error2)) {
-							hayError=true;
-							response.status(400).send({
-								message:"Error al crear paquete"
-							});
-							done();
-							return;
-							                    
-						}else{
-							productos.forEach(producto => {
-							let codigo = producto.codigo;
-							let cantidad = producto.cantidad;
-							client.query("INSERT INTO productos_paquete(codigo_producto,codigo_paquete,cantidad) VALUES($1,$2,$3)",[codigo,codigoPaquete,cantidad],(error3)=>{
-								if (shouldAbort(error3)) {
-									hayError=true;
-									return;                        
-									} 
-								})
-							});
-							if(!hayError){
-								client.query("COMMIT", (error4)=>{
-								if (shouldAbort(error4)) {
-										hayError=true;                      
-									}
-									response.status(200).send({
-										message:"Paquete creado exitosamente"
-									});
-									return;
-								});  
-								done()
-								return;						
-							}else{
-								shouldAbort(true);
-								response.status(400).send({
-									message:"Error al crear paquete"
-								});
-								done()
-								return;	 
-						}
-					}
-					});
-				
-				}
+	const actualizarPaquete = async (request, response) => {
+		const client = await pool.connect()
+		try {
+			let codigoPaquete = request.body.codigo;
+			let nombre = request.body.nombre;
+			let productos = request.body.productos;
+			let precio = request.body.precio;
+			await client.query('BEGIN');
+			await client.query('DELETE FROM paquetes WHERE codigo=$1',[codigoPaquete]);
+			await client.query("INSERT INTO paquetes(codigo,precio,nombre) VALUES($1,$2,$3)",[codigoPaquete,precio,nombre]);
+			productos.forEach(producto => {
+				let codigo = producto.codigo;
+				let cantidad = producto.cantidad;
+				await client.query("INSERT INTO productos_paquete(codigo_producto,codigo_paquete,cantidad) VALUES($1,$2,$3)",[codigo,codigoPaquete,cantidad]);
 			});
-			
-		}else{
-			done()
-			response.status(400).json({message:"Campos Faltantes"});
+			await client.query('COMMIT');
+			response.status(200).send({
+				message:"Paquete actualizado exitosamente"
+			});
+			return;
+		} catch (error) {
+			await client.query('ROLLBACK')
+			response.status(400).send({
+				message:"Error al actualizar paquete: "+error
+			});
+			return;
 		}
-		});   
-	};
+	}
+
+	const crearPaquete = async (request, response) => {
+		const client = await pool.connect()
+		try {
+			if(nombre && productos && precio && codigoPaquete){
+
+				let codigoPaquete = request.body.codigo;
+				let nombre = request.body.nombre;
+				let productos = request.body.productos;
+				let precio = request.body.precio;
+				await client.query('BEGIN');
+				await client.query("INSERT INTO paquetes(codigo,precio,nombre) VALUES($1,$2,$3)",[codigoPaquete,precio,nombre]);
+				productos.forEach(producto => {
+					let codigo = producto.codigo;
+					let cantidad = producto.cantidad;
+					await client.query("INSERT INTO productos_paquete(codigo_producto,codigo_paquete,cantidad) VALUES($1,$2,$3)",[codigo,codigoPaquete,cantidad]);
+				});
+				await client.query('COMMIT');
+				response.status(200).send({
+					message:"Paquete creado exitosamente"
+				});
+				return;
+			}else{
+				response.status(400).send({
+					message:"Paquete no se creo, complete la informacion"
+				});
+				return;
+			}
+		} catch (error) {
+			await client.query('ROLLBACK')
+			response.status(400).send({
+				message:"Error al crear paquete: "+error
+			});
+			return;
+		}
+	}
+
+	// const crearPaquete = (request, response) => {
+	// 	let codigoPaquete = request.body.codigo;
+	// 	let nombre = request.body.nombre;
+	// 	let productos = request.body.productos;
+	// 	let precio = request.body.precio;
+	// 	pool.connect((err, client, done)=>{
+	// 		let hayError = false;
+	// 		const shouldAbort = (err) => {
+	// 			if (err) {
+	// 			hayError=true;
+	// 			client.query('ROLLBACK', err2 => {
+	// 				if (err2) {
+	// 					console.log("Error al hacer rollback")
+	// 				}
+	// 			})
+	// 				return true;
+	// 			}
+	// 			else{
+	// 				return false;
+	// 			}
+	// 		}
+
+	// 	if(nombre && productos && precio && codigoPaquete){
+	// 		client.query("BEGIN", (error)=>{
+	// 		if (shouldAbort(error)) {
+	// 				response.status(400).send({
+	// 				message:"Error al crear paquete"
+	// 			});
+	// 				done();
+	// 				return;
+	// 			}else{
+	// 				client.query("INSERT INTO paquetes(codigo,precio,nombre) VALUES($1,$2,$3)",[codigoPaquete,precio,nombre], (error2)=>{
+	// 					if (shouldAbort(error2)) {
+	// 						hayError=true;
+	// 						response.status(400).send({
+	// 							message:"Error al crear paquete"
+	// 						});
+	// 						done();
+	// 						return;
+							                    
+	// 					}else{
+	// 						productos.forEach(producto => {
+	// 						let codigo = producto.codigo;
+	// 						let cantidad = producto.cantidad;
+	// 						client.query("INSERT INTO productos_paquete(codigo_producto,codigo_paquete,cantidad) VALUES($1,$2,$3)",[codigo,codigoPaquete,cantidad],(error3)=>{
+	// 							if (shouldAbort(error3)) {
+	// 								hayError=true;
+	// 								return;                        
+	// 								} 
+	// 							})
+	// 						});
+	// 						if(!hayError){
+	// 							client.query("COMMIT", (error4)=>{
+	// 							if (shouldAbort(error4)) {
+	// 									hayError=true;                      
+	// 								}
+	// 								response.status(200).send({
+	// 									message:"Paquete creado exitosamente"
+	// 								});
+	// 								return;
+	// 							});  
+	// 							done()
+	// 							return;						
+	// 						}else{
+	// 							shouldAbort(true);
+	// 							response.status(400).send({
+	// 								message:"Error al crear paquete"
+	// 							});
+	// 							done()
+	// 							return;	 
+	// 					}
+	// 				}
+	// 				});
+				
+	// 			}
+	// 		});
+	// 	}else{
+	// 		done()
+	// 		response.status(400).json({message:"Campos Faltantes"});
+	// 	}
+	// 	});   
+	// };
 
 	const getPaquetes = (request,response) =>{
 		pool.query("SELECT * FROM paquetes",(error,results)=>{
@@ -136,4 +199,4 @@
 
 
 
-	module.exports = {crearPaquete, getPaquetes, getProductosPaquete}
+	module.exports = {crearPaquete, getPaquetes, getProductosPaquete, actualizarPaquete}
