0cef5c87bae5e0423b7f0b31cb64cf4aee4af37f
David Fonseca <jcotta@Colsof.com>
2022-08-09 05:34:59 -0500

    Ajustes

--- src/controller/ventas.controller.js
+++ src/controller/ventas.controller.js
@@ -15,6 +15,31 @@ const pool = new Pool({
     }
     });
 
+    const getVentasCliente = (request,response) =>{
+        let cliente = request.body.cliente;
+        pool.query("select foo.id,foo.cliente,foo.nombre,foo.fecha,sum(foo.precio) as precio from \
+        (select ve.id, ve.fecha, ve.cliente, cl.nombre, sum((vp.cantidad*pa.precio)) as precio  from ventas ve \
+        inner join ventas_paquetes vp on vp.venta = ve.id \
+        inner join paquetes pa on pa.codigo=vp.paquete \
+        inner join clientes cl on cl.cedula=ve.cliente where ve.cliente=$1\
+        group by ve.id, ve.fecha, ve.cliente, cl.nombre \
+        union \
+        select ve2.id, ve2.fecha, ve2.cliente, c.nombre, sum(vp2.cantidad*p.precio) as precio from ventas ve2 \
+        inner join ventas_productos vp2 on ve2.id=vp2.venta \
+        inner join productos p on p.codigo=vp2.producto \
+        inner join clientes c on c.cedula=ve2.cliente where ve2.cliente=$2\
+        group by ve2.id, ve2.fecha, ve2.cliente, c.nombre) as foo group by foo.id,foo.fecha,foo.cliente,foo.nombre ORDER BY foo.fecha desc",[cliente,cliente],(error,results)=>{
+          if (error) {
+            response.status(500)
+                .send({
+                  message: error
+                });
+            }else{
+              response.status(200).send({ventas:results.rows});
+            }
+        })
+      }
+
     const getVentas = (request,response) =>{
         pool.query("select foo.id,foo.cliente,foo.nombre,foo.fecha,sum(foo.precio) as precio from \
         (select ve.id, ve.fecha, ve.cliente, cl.nombre, sum((vp.cantidad*pa.precio)) as precio  from ventas ve \
@@ -183,4 +208,4 @@ const registrarVentaProductos = async (request, response) => {
     }
 };
 
-module.exports = {registrarVentaProductos, getContenidoVentas, getVentas, eliminarVenta}
+module.exports = {registrarVentaProductos, getContenidoVentas, getVentas, eliminarVenta, getVentasCliente}
