6299f14626ea0f6b31b422c89d70eacb4ee36805
David Fonseca <jcotta@Colsof.com>
2022-08-29 23:58:36 -0500

    ajustes

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -56,7 +56,21 @@ const pool = new Pool({
           subject: "Notificacion de Deudas",
           text : "Prueba",
           html: `<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>
-          <p>Here's a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>`
+          <p>Here's a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>`,
+          amp: `<!doctype html>
+        <html ⚡4email>
+          <head>
+            <meta charset="utf-8">
+            <style amp4email-boilerplate>body{visibility:hidden}</style>
+            <script async src="https://cdn.ampproject.org/v0.js"></script>
+            <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+          </head>
+          <body>
+            <p><b>Hello</b> to myself <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
+            <p>No embedded image attachments in AMP, so here's a linked nyan cat instead:<br/>
+              <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
+          </body>
+        </html>`
         }
         console.log("Enviando")
         errores = await sendEmailPromise(mailData,errores,cliente);
