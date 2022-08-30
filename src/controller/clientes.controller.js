ee0a1bb89fee356069e346229812cf991c10f83a
David Fonseca <jcotta@Colsof.com>
2022-08-30 09:53:42 -0500

    ajuste

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -39,7 +39,7 @@ const pool = new Pool({
 
   const sendAllEmail = async (request, response) =>{
     try{
-      let query = "select c.cedula, c.nombre, c.email, sum(v.valor) as debito, q2.valor as abonos, q2.valor-sum(v.valor) as saldo from clientes c \
+      let query = "select c.cedula, c.nombre, c.email, cast(sum(v.valor) as money) as debito, cast(q2.valor as money) as abonos, cast(q2.valor-sum(v.valor) as money) as saldo from clientes c \
       left join ventas v on v.cliente = c.cedula \
       left join \
       (	select c2.cedula, sum(a.valor) as valor \
