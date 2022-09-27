d1c0f599d6f7b4be02ed744f66f01d59217e0e0c
David Fonseca <jcotta@Colsof.com>
2022-09-27 00:13:46 -0500

    Ajustes

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -389,7 +389,7 @@ const getClientes = (request,response) =>{
 }
 
 const getAbonos = (request,response) =>{
-  let query = "SELECT c.nombre, a.id, a.fecha, cast(a.valor as money) as valor, a.usuario, a.tipo FROM abonos inner join clientes c on c.cedula=a.cliente"
+  let query = "SELECT c.nombre, a.id, a.fecha, cast(a.valor as money) as valor, a.usuario, a.tipo FROM abonos a inner join clientes c on c.cedula=a.cliente"
   pool.query(query,(error,results)=>{
     if (error) {
       response.status(500)
