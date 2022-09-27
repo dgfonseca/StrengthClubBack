33eedf5f5c69561dcdbf52724ea69db65d39e8d6
David Fonseca <jcotta@Colsof.com>
2022-09-27 01:38:21 -0500

    Ajustes

--- src/controller/sesiones.controller.js
+++ src/controller/sesiones.controller.js
@@ -92,33 +92,33 @@ const crearSesionDeIcs =  async (request, response)=>{
       if(entrenadorRes.rowCount<1){
         response.status(400)
           .send({
-            message: "El entrenador "+ entrenador.replace("%",'') +" no existe",
+            message: "El entrenador "+ entrenador.replaceAll("%",'') +" no existe",
             code:1
           });
           return;
       }if(clienteRes.rowCount<1 ){
         response.status(400)
         .send({
-          message: "El cliente "+ cliente.replace("%",'') +" no existe",
+          message: "El cliente "+ cliente.replaceAll("%",'') +" no existe",
           code:2
         });
         return;
       }
       else{
-        cliente = clienteRes.rows[0].cedula
-        entrenador = entrenadorRes.rows[0].cedula
-        const countRes = await pool.query("SELECT COUNT(*) FROM SESIONES WHERE (entrenador=$1) AND TO_TIMESTAMP(fecha,'YYYY-MM-DD HH24:MI') BETWEEN TO_TIMESTAMP($2,'YYYY-MM-DD HH24:MI') AND TO_TIMESTAMP($3,'YYYY-MM-DD HH24:MI') + interval '74 minutes' ", [entrenador, fecha, fecha]);
-        const countRes2 = await pool.query("SELECT COUNT(*) FROM SESIONES WHERE (cliente=$1) AND TO_TIMESTAMP(fecha,'YYYY-MM-DD HH24:MI') BETWEEN TO_TIMESTAMP($2,'YYYY-MM-DD HH24:MI') AND TO_TIMESTAMP($3,'YYYY-MM-DD HH24:MI') + interval '74 minutes' ", [cliente, fecha, fecha]);
+        let cliente2 = clienteRes.rows[0].cedula
+        let entrenador2 = entrenadorRes.rows[0].cedula
+        const countRes = await pool.query("SELECT COUNT(*) FROM SESIONES WHERE (entrenador=$1) AND TO_TIMESTAMP(fecha,'YYYY-MM-DD HH24:MI') BETWEEN TO_TIMESTAMP($2,'YYYY-MM-DD HH24:MI') AND TO_TIMESTAMP($3,'YYYY-MM-DD HH24:MI') + interval '74 minutes' ", [entrenador2, fecha, fecha]);
+        const countRes2 = await pool.query("SELECT COUNT(*) FROM SESIONES WHERE (cliente=$1) AND TO_TIMESTAMP(fecha,'YYYY-MM-DD HH24:MI') BETWEEN TO_TIMESTAMP($2,'YYYY-MM-DD HH24:MI') AND TO_TIMESTAMP($3,'YYYY-MM-DD HH24:MI') + interval '74 minutes' ", [cliente2, fecha, fecha]);
         if(countRes.rows[0].count>0){
             response.status(400).send({
-              message: "Ya hay sesiones agendadas para el entrenador "+entrenador.replace("%",'')+" en el horario: "+fecha,
+              message: "Ya hay sesiones agendadas para el entrenador "+entrenador.replaceAll("%",'')+" en el horario: "+fecha,
               code: 3
           })
           return;
         }
         if(countRes2.rows[0].count>0){
             response.status(400).send({
-              message: "Ya hay sesiones agendadas para el cliente"+cliente.replace("%",'')+" en el horario: "+fecha,
+              message: "Ya hay sesiones agendadas para el cliente"+cliente.replaceAll("%",'')+" en el horario: "+fecha,
               code: 4
           })
           return;
