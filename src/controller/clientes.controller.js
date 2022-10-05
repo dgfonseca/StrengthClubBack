3e73107ece8c62190c20b99a0697157f47775c7b
David Fonseca <jcotta@Colsof.com>
2022-10-04 23:35:19 -0500

    Ajuste

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -64,7 +64,8 @@ const pool = new Pool({
       let sesionesHtml;
       if(cuenta.rows[0].anticipado){
         let sesionesPagadas = (parseFloat(sesionesVentasProductos.rows[0].sesiones)+parseFloat(sesionesVentasPaquetes.rows[0].sesiones))
-        console.log("Holaaaa"+sesionesTomadas.rows[0])
+        console.log("ASDAS")
+        console.log("Holaaaa"+sesionesTomadas)
         let sesionesRestantes = (sesionesPagadas-sesionesTomadas.rows[0].sesiones)
         let sesionesTomadas = (sesionesTomadas.rows[0].sesiones)
         let saldoTotalPre = parseFloat(deuda.rows[0].debito) - parseFloat(abonosValue.rows[0].abonos)
