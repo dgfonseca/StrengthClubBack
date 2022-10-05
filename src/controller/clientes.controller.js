dc71e1af7b81d09e16b98f62f8eac7e4ef56bfec
David Fonseca <jcotta@Colsof.com>
2022-10-04 23:21:58 -0500

    Ajuste

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -63,11 +63,10 @@ const pool = new Pool({
 
       let sesionesHtml;
       if(cuenta.rows[0].anticipado){
-        let sesionesPagadas = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(parseFloat(sesionesVentasProductos.rows[0].sesiones)+parseFloat(sesionesVentasPaquetes.rows[0].sesiones))
-        console.log(sesionesPagadas)
-        let sesionesRestantes = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(sesionesPagadas-sesionesTomadas.rows[0].sesiones)
-        let sesionesTomadas = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(sesionesTomadas.rows[0].sesiones)
-        let saldoTotal = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(deuda.rows[0].debito - abonosValue.rows[0].abonos)
+        let sesionesPagadas = (parseFloat(sesionesVentasProductos.rows[0].sesiones)+parseFloat(sesionesVentasPaquetes.rows[0].sesiones))
+        let sesionesRestantes = (sesionesPagadas-sesionesTomadas.rows[0].sesiones)
+        let sesionesTomadas = (sesionesTomadas.rows[0].sesiones)
+        let saldoTotal = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(parseFloat(deuda.rows[0].debito) - parseFloat(abonosValue.rows[0].abonos))
         let debito = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(deuda.rows[0].debito)
         let abonos = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(abonosValue.rows[0].abonos)
         sesionesHtml='<tr style="font-weight:bold"> \
