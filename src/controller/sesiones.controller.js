9b77f27ae7412bdbed90aadcbe11e94224e1e0ed
David Fonseca <jcotta@Colsof.com>
2022-10-12 12:04:11 -0500

    Ajuste

--- src/controller/sesiones.controller.js
+++ src/controller/sesiones.controller.js
@@ -215,8 +215,9 @@ const crearSesion = async (request, response) =>{
     let virtual = request.body.virtual;
     if(entrenador && cliente && fecha){
       try{
-        let clienteRes = await pool.query("SELECT anticipado from clientes where cedula=$1",[cliente])
+        let clienteRes = await pool.query("SELECT anticipado,precio_sesion from clientes where cedula=$1",[cliente])
         let results = await pool.query("SELECT COUNT(*) FROM SESIONES WHERE (entrenador=$1 OR cliente=$2) AND virtual=false AND TO_TIMESTAMP(fecha,'YYYY-MM-DD HH24:MI') BETWEEN TO_TIMESTAMP($3,'YYYY-MM-DD HH24:MI') AND TO_TIMESTAMP($4,'YYYY-MM-DD HH24:MI') + interval '74 minutes' ", [entrenador, cliente, fecha,fecha]);
+        let sesionPrecio = await pool.query("SELECT precio FROM productos WHERE codigo='SESV'")
         if(results.rows[0].count>0){
           response.status(400).send({
               message: "Ya hay sesiones agendadas en dicho horario"
@@ -225,6 +226,12 @@ const crearSesion = async (request, response) =>{
         }else{
           let sesion = await pool.query("INSERT INTO SESIONES(entrenador,cliente,fecha,asistio,virtual) VALUES($1,$2,$3,$4,$5) RETURNING id",[entrenador,cliente,fecha,asistio,virtual]);
           if(!clienteRes.rows[0].anticipado){
+            let precioSesion;
+            if(clienteRes.rows[0].precio_sesion!=null&&clienteRes.rows[0].precio_sesion!=undefined){
+              precioSesion=clienteRes.rows[0].precio_sesion;
+            }else{
+              precioSesion=sesionPrecio.rows[0].precio
+            }
             await pool.query("INSERT INTO ventas(cliente,fecha,valor,usuario,sesion) VALUES ($1,$2,$3,$4,$5) RETURNING id",[cliente,fecha,precioSesion,request.tokenData,sesion.rows[0].id]);
           }
           response.status(200).send({message:"Sesion Agendada Exitosamente"});
