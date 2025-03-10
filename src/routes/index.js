8803e97f2fc4063e2013304333f982792bcc8702
David Fonseca <dafonsec@redhat.com>
2025-03-10 16:42:16 -0500

    Ajuste sesiones timestamp

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ const contabilidad = require("../controller/contabilidad.controller")
 router = express.Router();
 router.use(function(req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers","Access-Control-Allow-Origin",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
